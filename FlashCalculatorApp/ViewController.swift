a740d21436752b65bb5a74ab50254b66a863efed
kurisu-seima <kurisuseima@gmail.com>
2021-01-05 23:30:36 +0900

    Initial Commit

---
+++ FlashCalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashCalculatorApp
+//
+//  Created by 栗須星舞 on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
