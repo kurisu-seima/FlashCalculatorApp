814aecde8a2ff77155352f0371eb885b335ce3f8
kurisu-seima <kurisuseima@gmail.com>
2021-01-17 23:46:32 +0900

    ストーリーボード上で答え合わせの画面だけ作成

--- FlashCalculatorApp/Controller/CheckAnswerViewController.swift
+++ FlashCalculatorApp/Controller/CheckAnswerViewController.swift
@@ -9,21 +9,11 @@ import UIKit
 
 class CheckAnswerViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = "\(FlashCalculatorManager.shared.numberOfDigit)桁 \(FlashCalculatorManager.shared.flashSpeed)秒間隔 \(FlashCalculatorManager.shared.numberOfQuestion)問"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
